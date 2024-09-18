83bb7bbf32d8da2c1e2c16985bf9d279762e6454
intigam <intiqamsumalov1@gmail.com>
2024-09-18 17:50:00 +0400

    second task

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -3,10 +3,11 @@ export default function taskBlock(trueOrFalse) {
     let task2 = true;
   
     if (trueOrFalse) {
-      let task = true;
-      let task2 = false;
+      let task = true;  
+      let task2 = false; 
+      return [task, task2];  
     }
   
-    return [task, task2];
+    return [task, task2];  
   }
   
