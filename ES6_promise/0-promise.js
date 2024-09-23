2e891a1c2f5eef5e880431087b487ddacd144017
intigam <intiqamsumalov1@gmail.com>
2024-09-23 16:57:02 +0400

    promise start task

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,14 +1,13 @@
 // Only make Promise
 function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-      /* eslint-disable */
-        if (true) {
-          resolve();
-        } else {
-          reject();
-        }
-        /* eslint-enable */
-    });
-  }
-  
-  export default getResponseFromAPI;
+  return new Promise((resolve, reject) => {
+    /* eslint-disable */
+    if (true) {
+      resolve();
+    } else {
+      reject();
+    }
+    /* eslint-enable */
+  });
+}
+export default getResponseFromAPI;
