c3f2535f794e9ac1dd9786c36303b5bf7d3b7f3c
intigam <intiqamsumalov1@gmail.com>
2024-09-24 18:17:23 +0400

    promise task complete

--- ES6_promise/8-try.js
+++ ES6_promise/8-try.js
@@ -1,6 +1,6 @@
 export default function divideFunction(numerator, denominator) {
   if (denominator === 0) {
-    throw Error("cannot divide by 0");
+    throw Error('cannot divide by 0');
   }
 
   const result = numerator / denominator;
