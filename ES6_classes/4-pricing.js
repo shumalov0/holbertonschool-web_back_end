46de9df8d4a77b5c2fade4ee776b4fef9e1cf304
intigam <intiqamsumalov1@gmail.com>
2024-09-27 09:42:50 +0400

    classes task complete

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -1,30 +1,30 @@
-// Pricing
-import Currency from './3-currency';
+
+import Currency from "./3-currency";
 
 export default class Pricing {
-  constructor(amount = '', currency) {
+  constructor(amount = "", currency) {
     this.amount = amount;
     this.currency = currency;
   }
 
   displayFullPrice() {
-    const code = (this.currency._code);
-    const name = (this.currency._name);
+    const code = this.currency._code;
+    const name = this.currency._name;
     const money = `${this.amount} ${name} (${code})`;
 
     return money;
   }
 
   static convertPrice(amount = 0, conversionRate = 0) {
-    if (typeof amount !== 'number') {
-      throw new TypeError('amount must be a string');
+    if (typeof amount !== "number") {
+      throw new TypeError("amount must be a string");
     }
 
-    if (typeof conversionRate !== 'number') {
-      throw new TypeError('conversionRate must be a string');
+    if (typeof conversionRate !== "number") {
+      throw new TypeError("conversionRate must be a string");
     }
 
-    return (amount * conversionRate);
+    return amount * conversionRate;
   }
 
   get amount() {
@@ -32,8 +32,8 @@ export default class Pricing {
   }
 
   set amount(value) {
-    if (typeof value !== 'number') {
-      throw new TypeError('amount must be a number');
+    if (typeof value !== "number") {
+      throw new TypeError("amount must be a number");
     }
     this._amount = value;
   }
@@ -44,7 +44,7 @@ export default class Pricing {
 
   set currency(value) {
     if (!(value instanceof Currency)) {
-      throw new TypeError('currency must be a Currency');
+      throw new TypeError("currency must be a Currency");
     }
     this._currency = value;
   }
