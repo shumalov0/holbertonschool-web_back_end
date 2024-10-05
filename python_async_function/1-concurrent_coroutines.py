dc7c679eea30b639ec0e78a3cdc34b602f014c8c
intigam <intiqamsumalov1@gmail.com>
2024-10-05 20:23:01 +0400

    async

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -10,7 +10,6 @@
     Arguments: n: int, max_delay: int = 10
 """
 import asyncio
-import random
 from typing import List
 wait_random = __import__("0-basic_async_syntax").wait_random
 async def wait_n(n: int, max_delay: int = 10) -> List[float]:
