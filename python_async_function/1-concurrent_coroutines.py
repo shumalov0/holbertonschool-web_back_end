34930d1eef005108a20b75576f47c5a9cc38bf67
intigam <intiqamsumalov1@gmail.com>
2024-10-05 23:34:38 +0400

    async

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
 """ Import wait_random and write a new function wait_n """
+
 import asyncio
 from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
+
+
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """ Create a sorted list with delay """
     tasks = []
     for i in range(n):
         task = asyncio.create_task(wait_random(max_delay))
         tasks.append(task)
-    list = [await task for task in asyncio.as_completed(tasks)]
-    return list
+
+    delays = [await task for task in asyncio.as_completed(tasks)]
+    return delays
