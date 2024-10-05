1f10e4c6218fff8541cdec8742bee62c53478a49
intigam <intiqamsumalov1@gmail.com>
2024-10-05 20:54:46 +0400

    async

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,25 +1,22 @@
 #!/usr/bin/env python3
-""" Deatils: Import wait_random from the previous python file that
-                 you’ve written and write an async routine called wait_n
-                 that takes in 2 int arguments: max_delay and n. You will
-                 spawn wait_random n times with the specified max_delay.
-
-                 wait_n should return the list of all the delays(float values)
-                 The list of the delays should be in ascending order without
-                 using sort() because of concurrency.
-    Arguments: n: int, max_delay: int = 10
-"""
+''' Run multiple coroutines concurrently with asyncio '''
 import asyncio
 from typing import List
-wait_random = __import__("0-basic_async_syntax").wait_random
-async def wait_n(n: int, max_delay: int = 10) -> List[float]:
-    """Waits for ran delay until max_delay, returns list of actual delays"""
-    spawn_ls = []
-    delay_ls = []
-    for i in range(n):
-        delayed_task = asyncio.create_task(wait_random(max_delay))
-        delayed_task.add_done_callback(lambda x: delay_ls.append(x.result()))
-        spawn_ls.append(delayed_task)
-    for spawn in spawn_ls:
-        await spawn
-    return delay_ls
+
+# Import the wait_random function from the previous file/module
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    '''
+    Execute wait_random 'n' times with concurrency and return sorted delays.
+    '''
+    tasks: List[asyncio.Task] = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    delays: List[float] = []
+
+    # Collect results as tasks complete in the order they finish
+    for task in asyncio.as_completed(tasks):
+        delay = await task
+        delays.append(delay)
+    
+    # Return the delays sorted in ascending order (natural sorting happens due to async completion)
+    return delays
