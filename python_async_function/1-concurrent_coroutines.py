89260a5916062cadb006869fcdc0ef0e73dd83e1
intigam <intiqamsumalov1@gmail.com>
2024-10-05 21:08:18 +0400

    async

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,8 +1,6 @@
-#!/usr/bin/env python3
 """ Import wait_random and write a new function wait_n """
 import asyncio
 from typing import List
-
 wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int) -> List[float]:
     '''Create a sorted list with delay '''
