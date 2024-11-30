afc87263047916ab17a5e16f231e744dad83bfbd
intigam <intiqamsumalov1@gmail.com>
2024-12-01 00:29:18 +0400

    auth

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -13,33 +13,32 @@ class BasicAuth(Auth):
 
     def extract_base64_authorization_header(
             self, authorization_header: str) -> str:
-        ''' def extract base64 authorization header '''
+        ''' Extract base64 authorization header '''
         if authorization_header is None:
             return None
-        if type(authorization_header) != str:
+        if not isinstance(authorization_header, str):
             return None
         if authorization_header.startswith("Basic "):
             return "".join(authorization_header.split(" ")[1:])
+        return None
 
     def decode_base64_authorization_header(
             self, base64_authorization_header: str) -> str:
-        ''' def decode base 64 authorization '''
-        if base64_authorization_header and type(
-                base64_authorization_header) == str:
+        ''' Decode base64 authorization header '''
+        if base64_authorization_header and isinstance(base64_authorization_header, str):
             try:
-                x = base64_authorization_header.encode('utf-8')
-                base = base64.b64decode(x)
-                return base.decode('utf-8')
+                encoded = base64_authorization_header.encode('utf-8')
+                decoded = base64.b64decode(encoded)
+                return decoded.decode('utf-8')
             except Exception:
                 return None
+        return None
 
     def extract_user_credentials(
             self, decoded_base64_authorization_header: str) -> (str, str):
-        ''' return the user mail and the password '''
-        c = decoded_base64_authorization_header
-        if c and type(c) == str and ":" in c:
-            mail = c.split(':')[0]
-            password = "".join(c.split(':', 1)[1:])
+        ''' Return the user email and the password '''
+        if decoded_base64_authorization_header and isinstance(decoded_base64_authorization_header, str) and ":" in decoded_base64_authorization_header:
+            mail, password = decoded_base64_authorization_header.split(':', 1)
             return mail, password
         return None, None
 
@@ -48,16 +47,16 @@ class BasicAuth(Auth):
             user_email: str,
             user_pwd: str
     ) -> TypeVar('User'):
-        """do funny stuff"""
+        """ Get User object from credentials """
         if (
                 user_email is None or
                 user_pwd is None or
-                type(user_email) is not str or
-                type(user_pwd) is not str
+                not isinstance(user_email, str) or
+                not isinstance(user_pwd, str)
         ):
             return None
         user = User()
-        search = user.search({"email": user_email, })
+        search = user.search({"email": user_email})
         if not search:
             return None
         user = search[0]
@@ -68,8 +67,7 @@ class BasicAuth(Auth):
         return user
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """ current_user
-        """
+        """ Get current user """
         auth = self.authorization_header(request)
         if auth is None:
             return None
