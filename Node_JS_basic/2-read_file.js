bd04a134ea43a6cc673455dfac890c99990550e2
intigam <intiqamsumalov1@gmail.com>
2024-11-30 22:27:36 +0400

    change package,json

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,4 +1,4 @@
-const fs = require("fs");
+const fs = require('fs');
 
 function countStudents(path) {
   let content;
@@ -6,17 +6,17 @@ function countStudents(path) {
   try {
     content = fs.readFileSync(path);
   } catch (err) {
-    throw new Error("Cannot load the database");
+    throw new Error('Cannot load the database');
   }
 
-  content = content.toString().split("\n");
+  content = content.toString().split('\n');
 
   let students = content.filter((item) => item);
 
-  students = students.map((item) => item.split(","));
+  students = students.map((item) => item.split(','));
 
-  const NUMBER_OF_STUDENTS = students.length ? students.length - 1 : 0;
-  console.log(`Number of students: ${NUMBER_OF_STUDENTS}`);
+  const studetns_number = students.length ? students.length - 1 : 0;
+  console.log(`Number of students: ${studetns_number}`);
 
   const fields = {};
   for (const i in students) {
@@ -33,7 +33,7 @@ function countStudents(path) {
     console.log(
       `Number of students in ${key}: ${fields[key].length}. List: ${fields[
         key
-      ].join(", ")}`
+      ].join(', ')}`,
     );
   }
 }
